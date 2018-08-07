9206b7f9008f67898bb8b9e351573f07e566dbb5
macfisher <macfisher01@gmail.com>
2018-08-07 16:52:18 -0600

    App config. properties with Java properties file and YAML

--- src/main/java/com/macfisher/PageController.java
+++ src/main/java/com/macfisher/PageController.java
@@ -2,6 +2,7 @@ package com.macfisher;
 
 import com.abc.foo.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,17 +14,29 @@ public class PageController
 
     private NotificationService notificationService;
 
+    @Value("${pageController.msg}")
+    private String pageControllerMsg;
+
+    @Value("${my.secret}")
+    private String mySecret;
+
     @Autowired
     public PageController(NotificationService notificationService) {
         this.notificationService = notificationService;
     }
-
-    @RequestMapping("/")
-    public String home() { return notificationService.toString(); }
 
     /*@Autowired // injects NotificationService into setter function
     public void setNotificationService(NotificationService notificationService)
     {
         this.notificationService = notificationService;
     }*/
+
+    @RequestMapping("/")
+    public String home() { return notificationService.toString(); }
+
+    @RequestMapping("message")
+    public String message() { return pageControllerMsg; }
+
+    @RequestMapping("secret")
+    public String secret() { return mySecret; }
 }
